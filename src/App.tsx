2eb8487fcb30a6a99d43fbd0167358ede39e77e9
anominy <140955338+anominy@users.noreply.github.com>
2026-02-21 22:41:20 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { JSX } from "react";
+
+export default function App(): JSX.Element {
+  return (
+    <div>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from   "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: path.resolve(__dirname, "src"),
+  publicDir: path.resolve(__dirname, "img"),
+  server: {
+    port: 8080,
+    strictPort: true
+  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: path.resolve(__dirname, "out"),
+    emptyOutDir: true,
+    minify: true,
+    sourcemap: false
+  }
+});
